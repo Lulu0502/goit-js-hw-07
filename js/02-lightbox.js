3450cfc852fd9e1ca963ebb15ef38843d18b47ad
Lulu0502 <suliya85@gmail.com>
2022-09-25 18:06:02 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,9 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
+const markup = galleryItems.map(
+    ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" 
+        alt="${description}" />
+        </a>`
+)
+    .join("");
 
+gallery.insertAdjacentHTML("beforeend", markup);
+gallery.addEventListener("click", onItemClick);
 
+function onItemClick(e) {
+    e.preventDefault();
 
+    var lightbox = new SimpleLightbox(".gallery a", {
+        captionData: "alt",
+        captionDelay: 250,
+    })
+}
 
 console.log(galleryItems);
 
